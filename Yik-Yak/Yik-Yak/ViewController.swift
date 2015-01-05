303bbe996fa72a2805907cc466c431af7112d75d
deion1george <deion1@optonline.net>
2015-01-05 02:56:03 -0500

    add all the files from xcode

---
+++ Yik-Yak/Yik-Yak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yik-Yak
+//
+//  Created by Deion George on 1/5/15.
+//  Copyright (c) 2015 Wedgewood Communication. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Yik-Yak/Yik-YakTests/Yik_YakTests.swift
@@ -0,0 +1,36 @@
+//
+//  Yik_YakTests.swift
+//  Yik-YakTests
+//
+//  Created by Deion George on 1/5/15.
+//  Copyright (c) 2015 Wedgewood Communication. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Yik_YakTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
